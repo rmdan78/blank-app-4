18a6903efc70d8625b71c3977147fd5b999c5777
rmdan78 <mhmhd.ramdan@yahoo.com>
2024-09-15 08:38:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,20 @@ import cv2
 # Coba berbagai indeks jika kamera default tidak berfungsi
 camera_index = 0
 cap = cv2.VideoCapture(camera_index)
-
-if not cap.isOpened():
-    print(f"Tidak dapat mengakses kamera dengan indeks {camera_index}")
-    camera_index =+ 1
-else:
-    print(f"Kamera terdeteksi dengan indeks {camera_index}")
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        cv2.imshow("Kamera", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
+while True:
+    if not cap.isOpened():
+        print(f"Tidak dapat mengakses kamera dengan indeks {camera_index}")
+        camera_index =+ 1
+    else:
+        print(f"Kamera terdeteksi dengan indeks {camera_index}")
+        break
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            cv2.imshow("Kamera", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    
+        cap.release()
    
